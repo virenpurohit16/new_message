151b497ed100de4a6bac5237eba82cc11da65b6d
Viren Purohit <virenpurohit16@gmail.com>
2022-03-11 12:18:20 +0530

    Adding message resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {miinimum:4, maximum: 15}
   has_secure_password
+  has_many :messages
 end
