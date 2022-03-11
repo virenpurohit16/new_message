394c72e92a56697602aceded12257a9666f5977f
Viren Purohit <virenpurohit16@gmail.com>
2022-03-11 11:54:42 +0530

    Adding User Resource

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {miinimum:4, maximum: 15}
+  has_secure_password
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username:"User1", password:"password")
+User.create(username:"User2", password:"password")
+User.create(username:"User3", password:"password")
+User.create(username:"User4", password:"password")
+User.create(username:"User6", password:"password")
